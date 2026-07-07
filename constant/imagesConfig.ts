7cd3b0a5ec7131ba8418633fba8e81940cc9b09e
Shek Rasel <swe.rasel@gmail.com>
2026-07-07 18:44:08 +0600

    added more images(contact page)

--- constant/imagesConfig.ts
+++ constant/imagesConfig.ts
@@ -1,3 +1,6 @@
 export const IMAGE = {
   aboutHeroImage: "/images/about_hero_image.avif",
+  london: "/images/london.avif",
+  france: "/images/france.avif",
+  germany: "/images/germany.avif",
 } as const;
