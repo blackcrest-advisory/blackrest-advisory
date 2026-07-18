d8a37cd726f55a81574c7b167495d0092bc6c8ee
Shek Rasel <swe.rasel@gmail.com>
2026-07-18 16:04:29 +0600

    fixed desing issue

--- components/shared/ClientMobileSidebar.tsx
+++ components/shared/ClientMobileSidebar.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect } from "react";
 import { motion } from "framer-motion";
-import { X } from "lucide-react";
-import ClientSidebar from "@/components/shared/ClientSidebar";
+import { Search, X } from "lucide-react";
+import ClientDesktopSidebar from "@/components/shared/ClientDesktopSidebar";
+import Logo from "@/components/shared/Logo";
 
 interface MobileSidebarProps {
   isOpen: boolean;
@@ -34,7 +35,7 @@ export default function ClientMobileSidebar({
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.2 }}
-        className="fixed inset-0 bg-black/40 backdrop-blur-sm z-40 lg:hidden"
+        className="fixed inset-0 bg-black/40 backdrop-blur-sm z-40 lg:hidden "
         onClick={onClose}
       />
 
@@ -44,18 +45,27 @@ export default function ClientMobileSidebar({
         animate={{ x: 0 }}
         exit={{ x: "-100%" }}
         transition={{ type: "spring", damping: 25, stiffness: 300 }}
-        className="fixed top-0 left-0 h-full w-[280px] z-50 shadow-2xl lg:hidden"
-        style={{ backgroundColor: "var(--color-card-bg)" }}
+        className="fixed top-0 left-0 h-full w-[280px] z-50 shadow-2xl lg:hidden bg-background"
+      
       >
         {/* Close button inside drawer */}
-        <button
-          onClick={onClose}
-          className="absolute top-3 right-3 p-2 rounded-md hover:bg-muted/50 transition-colors"
-          aria-label="Close menu"
-        >
-          <X size={22} style={{ color: "var(--color-foreground)" }} />
-        </button>
-        <ClientSidebar />
+        <div className="relative flex justify-between mt-4 px-2">
+          <Logo />
+
+          <div className="flex items-center gap-2">
+            <Search size="20" />
+            <button
+              onClick={onClose}
+              className="flex items- justify-between cursor-pointer"
+            >
+              <X size={22} style={{ color: "var(--color-foreground)" }} />
+            </button>
+          </div>
+        </div>
+
+        <div className="h-full">
+          <ClientDesktopSidebar />
+        </div>
       </motion.aside>
     </>
   );
