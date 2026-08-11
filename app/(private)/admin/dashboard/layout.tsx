b3a6274dc8bf78494bb6764829c6db80042e77e9
Shek Rasel <swe.rasel@gmail.com>
2026-08-11 10:39:54 +0600

    modifyed admin layout , get user

--- app/(private)/admin/dashboard/layout.tsx
+++ app/(private)/admin/dashboard/layout.tsx
@@ -1,9 +1,32 @@
 import DashboardLayout from "@/components/shared/DashboardLayout";
+import { CurrentUserProvider } from "@/app/providers/CurrentUserProvider";
+import { getCurrentUser } from "@/lib/utils/auth-utils";
+import { prisma } from "@/lib/db/client";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  const user = await getCurrentUser();
+  const currentUser = user
+    ? await prisma.user.findUnique({
+        where: {
+          id: user.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          avatarUrl: true,
+        },
+      })
+    : null;
+
+  return (
+    <CurrentUserProvider user={currentUser}>
+      <DashboardLayout>{children}</DashboardLayout>
+    </CurrentUserProvider>
+  );
 }
